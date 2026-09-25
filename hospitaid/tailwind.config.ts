efed26a301f818ab3643d12de45eb8b09e03b7f2
obianglilm <obianglilm@gmail.com>
2026-09-26 00:03:14 +0100

    Add files via upload

---
+++ hospitaid/tailwind.config.ts
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0577BE",
+        "primary-deep": "#04578F",
+        coral: "#E14547",
+      },
+    },
+  },
+  plugins: [],
+};
